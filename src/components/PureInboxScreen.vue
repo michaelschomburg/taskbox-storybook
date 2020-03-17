c3d27d926e7db4895ffe14094134d1b41f095dc2
michael_schomburg <michael.schomburg@studydrive.net>
2020-03-17 13:21:22 +0100

    tutorial done until test

---
+++ src/components/PureInboxScreen.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "PureInboxScreen"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PureTaskList.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "PureTaskList"
+    }
+</script>
+
+<style scoped>
+
+</style>
